563e7d1ecd05e47fea70d65edf959e34ec9be64b
Walter White <WalterWhite@walterjwhite.com>
2019-09-04 21:33:48 -0400

    merged

--- impl/src/main/java/com/walterjwhite/property/impl/source/PropertyValue.java
+++ impl/src/main/java/com/walterjwhite/property/impl/source/PropertyValue.java
@@ -2,8 +2,10 @@ package com.walterjwhite.property.impl.source;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Data
+@ToString(doNotUseGetters = true)
 @RequiredArgsConstructor
 public class PropertyValue {
   // protected final Class<? extends ConfigurableProperty> configurablePropertyClass;
